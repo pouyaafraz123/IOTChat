6c284164187f47756a424abc37eb8174b4a3f4b9
pouyaafraz123 <pouyaafraz123@gmail.com>
2023-05-30 22:42:00 +0330

    change database url

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,1 +1,3 @@
-export const DATABASE_URL = "mongodb://localhost:27017/IOTChat?retryWrites=true&w=majority";
+//export const DATABASE_URL = "mongodb://localhost:27017/IOTChat?retryWrites=true&w=majority";
+export const DATABASE_URL = "mongodb://root:03EabjzR00CQUZlo3rCZVXiY@esme.iran.liara.ir:33132/my-app?authSource=admin";
+
